d52370730136370eab19792e076349f7819de8bb
Ash Berlin <ash_github@firemirror.com>
2014-12-22 15:23:52 +0000

    Add in back-compat shim for existing cli argumaent form
    This is currently used in this form via .kitchen.yaml (for instance <https://github.com/ministryofjustice/metrics-formula/blob/d87a59f8b7e86378e0a310c8b47e5393e131d3d2/.kitchen.yml#L25>) and having this shim makes the upgrade path a lot easier.

--- src/shaker/command_line.py
+++ src/shaker/command_line.py
@@ -17,8 +17,7 @@ class ShakerCommandLine(object):
         subparsers = parser.add_subparsers()
 
         common_args = argparse.ArgumentParser(add_help=False)
-        common_args.add_argument('--root_dir',
-            default='.', help="Working path to operate under")
+        common_args.add_argument('--root_dir', default='.', help="Working path to operate under")
         common_args.add_argument('--root_constraint', default=argparse.SUPPRESS)
         common_args.add_argument('--root_formula', default=argparse.SUPPRESS)
 
@@ -30,11 +29,20 @@ class ShakerCommandLine(object):
         parser_update.set_defaults(force=True)
         parser_update.set_defaults(func=self.shake)
 
-        args_ns = parser.parse_args(args=cli_args)
+        args_ns = parser.parse_args(args=self.back_compat_args_fix(cli_args))
         # Convert the args as Namespace to dict a so we can pass it as kwargs to a function
         args = vars(args_ns)
 
         return args.pop('func')(**args)
+
+    def back_compat_args_fix(self, cli_args):
+        args = []
+        for arg in cli_args:
+            if arg.startswith("root_") and "=" in arg:
+                arg = "--" + arg
+            args.append(arg)
+
+        return args
 
     def shake(self, **kwargs):
         salt_shaker.shaker(**kwargs)
