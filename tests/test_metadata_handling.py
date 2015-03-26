5218cae7f7cedda5931fc8244361489e116a00cf
Niall Creech <niall.creech@digital.justice.gov.uk>
2015-03-26 10:30:49 +0000

    Issue #14: Duplicates in metadata.yml Removed unused setup and teardown methods in test_metadata_handling Updated test data to use dictionaries rather than load yaml Cleaned up some whitespace

--- tests/test_metadata_handling.py
+++ tests/test_metadata_handling.py
@@ -2,57 +2,50 @@ import unittest
 import yaml
 from shaker import helpers
 
+
 class TestMetadataHandling(unittest.TestCase):
-    
+
     # Sample metadata with duplicates
-    _sample_metadata_duplicates = \
-"""
-dependencies:
-    - git@github.com:test_organisation/test1-formula.git
-    - git@github.com:test_organisation/test1-formula.git
-    - git@github.com:test_organisation/test2-formula.git
-    - git@github.com:test_organisation/test3-formula.git
-    - git@github.com:test_organisation/test3-formula.git
-entry:
-    - dummy
-"""
-    _sample_metadata_no_duplicates = \
-"""
-dependencies:
-    - git@github.com:test_organisation/test1-formula.git
-    - git@github.com:test_organisation/test2-formula.git
-    - git@github.com:test_organisation/test3-formula.git
-entry:
-    - dummy
-"""
+    _sample_metadata_duplicates = {
+        "dependencies": [
+            "git@github.com:test_organisation/test1-formula.git",
+            "git@github.com:test_organisation/test1-formula.git",
+            "git@github.com:test_organisation/test2-formula.git",
+            "git@github.com:test_organisation/test3-formula.git",
+            "git@github.com:test_organisation/test3-formula.git"
+        ],
+        "entry": ["dummy"]
+    }
+
+    _sample_metadata_no_duplicates = {
+        "dependencies": [
+            "git@github.com:test_organisation/test1-formula.git",
+            "git@github.com:test_organisation/test2-formula.git",
+            "git@github.com:test_organisation/test3-formula.git"
+        ],
+        "entry": ["dummy"]
+    }
+
     # Sample config that yaml load will fail to parse
-    _sample_metadata_bad_yaml = \
-    """
-    dependencies--
-        - git@github.com:test_organisation/test1-formula.git
-        - git@github.com:test_organisation/test1-formula.git
-        - git@github.com:test_organisation/test2-formula.git
-        - git@github.com:test_organisation/test3-formula.git
-        - git@github.com:test_organisation/test3-formula.git
-    entry--
-        - dummy
-    """
-    @classmethod
-    def setup_class(cls):
-        pass
-    
-    @classmethod
-    def teardown_class(cls):
-        pass
-    
+    _sample_metadata_bad_yaml = {
+        "dependencies": [
+            "git@github.com:test_organisation/test1-formula.git",
+            "git@github.com:test_organisation/test1-formula.git",
+            "git@github.com:test_organisation/test2-formula.git",
+            "git@github.com:test_organisation/test3-formula.git",
+            "git@github.com:test_organisation/test3-formula.git"
+        ],
+        "entry": ["dummy"]
+    }
+
     def test_resolve_metadata_duplicates(self):
         """
         Check if we successfully remove duplicates from a sample metadata
         """
-        original_metadata = yaml.load(self._sample_metadata_duplicates)
-        expected_metadata = yaml.load(self._sample_metadata_no_duplicates)
+        original_metadata = self._sample_metadata_duplicates
+        expected_metadata = self._sample_metadata_no_duplicates
         resolved_metadata = helpers.resolve_metadata_duplicates(original_metadata)
-        
+
         expected_metadata_dependencies = expected_metadata["dependencies"]
         resolved_metadata_dependencies = resolved_metadata["dependencies"]
         expected_metadata_entries = expected_metadata["entry"]
@@ -63,18 +56,18 @@ entry:
             self.assertTrue(expected_metadata_dependency in resolved_metadata_dependencies, 
                             "test_resolve_metadata_duplicates: dependency '%s' not found in de-duplicated metadata"
                             % (expected_metadata_dependency))
-        
+
         # Test entry found
         for expected_metadata_entry in expected_metadata_entries:
             self.assertTrue(expected_metadata_entry in resolved_metadata_entries, 
                             "test_resolve_metadata_duplicates: Entry '%s' not found in de-duplicated metadata"
                             % (expected_metadata_entry))
-            
+
     def test_resolve_metadata_duplicates_bad_metadata_object(self):
         """
         Check if resolve_metadata_duplicates can handle bad yaml
         data without breaking anything
         """
-        expected = yaml.load(self._sample_metadata_bad_yaml)
-        result = helpers.resolve_metadata_duplicates(yaml.load(self._sample_metadata_bad_yaml))
+        expected = self._sample_metadata_bad_yaml
+        result = helpers.resolve_metadata_duplicates(self._sample_metadata_bad_yaml)
         self.assertEqual(result, expected, "")
