11c3a338517d165e54dd96b9f9dd6fa8d3ad9a3f
Ash Berlin <ash_github@firemirror.com>
2015-05-22 16:25:21 +0100

    v0.1.2 - This time with setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='salt-shaker',
-    version='0.0.1',
+    version='0.1.2',
     packages=['shaker'],
     url='http://github.com/ministryofjustice/salt_shaker',
     license='',
